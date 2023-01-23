50169382b5da85270f8d5579882eb753791f2f5b
Andrew Morrison <morrison.andrew422@gmail.com>
2023-01-23 17:08:57 -0500

    added my_select and fixed my_each

--- enumerables.rb
+++ enumerables.rb
@@ -1,17 +1,43 @@
+require 'byebug'
 class Array
+
 	def my_each(&blk)
 		i = 0
 		while i < self.length  
-			blk.call(self[i])
+			self[i] = blk.call(self[i])
 			i += 1
 		end
 		self
 	end
+
+	def my_select(&blk)
+		# debugger
+		ans = []
+
+		self.my_each do |ele|
+			ans << blk.call(ele)
+		end
+
+		# arr = self.my_each(blk.call)
+		# i = 0
+		# while i < arr.length
+		# 	if blk.call(arr[i])
+		# 	ans << arr[i]
+		# 	end
+		# end
+
+
+
+		ans
+	end
+
 end
 
-my_arr = [1, 2, 3]
+my_arr = [1, 2, 3, 4, 5]
 #blk = { |ele| puts ele } 
 p my_arr.my_each { |ele| ele } 
+
+p my_arr.my_select { |ele| ele == 2 || ele == 5 }
 
 
 
