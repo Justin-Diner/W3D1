5cf06ffdfcd45c4dd55ec5deb4b27e047389729b
Justin <Jcdiner@gmail.com>
2023-01-23 17:33:17 -0500

    added my_any?

--- enumerables.rb
+++ enumerables.rb
@@ -17,27 +17,40 @@ class Array
 		self.my_each do |ele|
 			ans << blk.call(ele)
 		end
-
-		# arr = self.my_each(blk.call)
-		# i = 0
-		# while i < arr.length
-		# 	if blk.call(arr[i])
-		# 	ans << arr[i]
-		# 	end
-		# end
-
-
-
 		ans
+	end
+
+	def my_reject(&blk)
+		out_arr = []
+
+		self.my_each do |ele|
+			if !blk.call(ele)
+				out_arr << ele
+			end
+		end
+		out_arr
+	end
+
+	def my_any?(&blk)
+		out_arr = []
+
+		self.my_each do |ele|
+			out_arr << ele if !blk.call(ele)
+		end
+		self.length != out_arr.length
 	end
 
 end
 
 my_arr = [1, 2, 3, 4, 5]
-#blk = { |ele| puts ele } 
-p my_arr.my_each { |ele| ele } 
+# blk = { |ele| puts ele } 
+# p my_arr.my_each { |ele| ele } 
+# p my_arr.my_select { |ele| ele == 2 || ele == 5 }
+# p my_arr.my_reject { |ele| ele < 3}
+p my_arr.my_any? {|ele| ele > 5}
 
-p my_arr.my_select { |ele| ele == 2 || ele == 5 }
+
+
 
 
 
